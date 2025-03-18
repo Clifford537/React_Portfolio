8d725eb82528aca1b1e267ba76f4c5020028c2d2
Clifford537 <cliffordmukosh@gmail.com>
2025-03-17 22:17:49 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HelloWorld from './components/Navbar'
+
+function App(){
+
+  return(
+    <>
+    <HelloWorld/>
+    </>
+  )
+
+}
+
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/styles.css'
+
+function HelloWorld(){
+     return(
+        <div  className="main">
+        <h1>Hello Devs ! 😊</h1>
+        </div>
+     )
+}
+
+
+export default HelloWorld;
